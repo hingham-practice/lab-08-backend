eef40833c5764e0da1b76640a98b208502e3815f
hingham <hannah.c.ingham@gmail.com>
2018-10-25 22:35:53 -0700

    added sql functionality for moviesDB and yelp

--- schema.sql
+++ schema.sql
@@ -15,3 +15,27 @@ CREATE TABLE weathers (
     time VARCHAR(255),
     location_id INTEGER NOT NULL REFERENCES locations(id)
 );
+
+CREATE TABlE yelps (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    image_url VARCHAR(255),
+    price VARCHAR(255),
+    rating NUMERIC(8),
+    url VARCHAR(255),
+    location_id INTEGER NOT NULL REFERENCES locations(id)
+
+);
+
+CREATE TABlE movies (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    overview VARCHAR(255),
+    average_votes NUMERIC(8),
+    total_votes NUMERIC(8),
+    popularity VARCHAR(255),
+    released_on VARCHAR(255),
+    image_url VARCHAR(255),
+    location_id INTEGER NOT NULL REFERENCES locations(id)
+
+);
